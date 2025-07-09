25815592453a4aa6201e462d125e8d93f46b453f
Flaviocorreia02 <flaviojesje@gmail.com>
2025-07-09 22:18:44 -0100

    Initial Commit

---
+++ wp-content/uploads/uag-plugin/assets/5000/uag-js-5476.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e62kkxqm' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-lecvs2dn' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-t5wvjb8c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-v7yfjl1c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-2n5dfxvx' );
+});
+ });
