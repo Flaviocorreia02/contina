07c5c062831d244979a4915dcb502344c2da158a
Flaviocorreia02 <flaviojesje@gmail.com>
2025-07-21 19:36:15 -0100

    Banner and somes improvement

--- wp-content/uploads/uag-plugin/assets/4000/uag-js-3610.js
+++ wp-content/uploads/uag-plugin/assets/4000/uag-js-3610.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-4c214b60 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-4c214b60 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-4c214b60 .swiper-button-next","prevEl":".uagb-block-4c214b60 .swiper-button-prev"}}	);
+});
+
+window.addEventListener("DOMContentLoaded", function(){
 	UAGBForms.init( {"block_id":"t3qihrvf","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"abc@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-t3qihrvf', 3610 );
 });
 window.addEventListener( 'load', function() {
